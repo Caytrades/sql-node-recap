525a34a67c9b1f8f675a09e4af31f5f0e1de1574
Caytrades <nanakwameopokuababio@gmail.com>
2024-11-13 15:57:25 +0000

    Where Added

---
+++ where.js
@@ -0,0 +1,18 @@
+var mysql = require('mysql');
+require('dotenv').config()
+
+var con = mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  /*Select all customers with the address "Park Lane 38":*/
+  con.query("SELECT * FROM customers WHERE address = 'Park Lane 38'", function (err, result) {
+    if (err) throw err;
+    console.log(result);
+  });
+});
